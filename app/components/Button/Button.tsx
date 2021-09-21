b371b09d1f2e0a409e649285c7b6854ba7f7e44f
Eddy Rivera Echegaray <eddyrivera@Eddys-MacBook-Pro-2.local>
2021-09-21 11:30:06 -0300

    feat: button, welcome screen and view image screen

---
+++ app/components/Button/Button.tsx
@@ -0,0 +1,40 @@
+import React, { ReactNode } from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+import colors from "../../config/colors";
+
+interface IButton extends TouchableOpacityProps {
+  color: "primary" | "secondary";
+  children: ReactNode;
+}
+
+const Button = ({ color, children, onPress }: IButton) => {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: colors[color] }]}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>{children}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    width: "100%",
+    padding: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 25,
+  },
+  text: {
+    color: colors.white,
+    fontSize: 18,
+  },
+});
---
+++ app/screens/WelcomeScreen.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { View, Text, ImageBackground, StyleSheet, Image } from "react-native";
+import Button from "../components/Button";
+
+const WelcomeScreen = () => {
+  return (
+    <ImageBackground
+      blurRadius={8}
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoCnntainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text style={styles.tagLine}>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <Button color="primary" onPress={() => console.log("lala")}>
+          LOGIN
+        </Button>
+        <View style={{ marginTop: 20 }} />
+        <Button color="secondary" onPress={() => console.log("lala")}>
+          REGISTER
+        </Button>
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  buttonsContainer: {
+    padding: 20,
+    width: "100%",
+  },
+  logoCnntainer: {
+    alignItems: "center",
+    position: "absolute",
+    top: 70,
+  },
+  tagLine: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingTop: 20,
+  },
+});
+
+export default WelcomeScreen;
