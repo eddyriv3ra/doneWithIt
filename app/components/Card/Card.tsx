cd21822ad17a46958ee0d78847e37cf902146034
Eddy Rivera Echegaray <eddyrivera@Eddys-MacBook-Pro-2.local>
2021-09-21 12:10:28 -0300

    feat: card component

---
+++ app/components/Card/Card.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, StyleSheet, Image, Text, ImageProps } from "react-native";
+import colors from "../../config/colors";
+
+interface ICard {
+  title: string;
+  subTitle: string;
+  image: ImageProps;
+}
+
+const Card = ({ title, subTitle, image }: ICard) => {
+  return (
+    <View style={styles.card}>
+      <Image style={styles.image} source={image} />
+      <View style={styles.detailsContainer}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subTitle}>{subTitle}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 15,
+    backgroundColor: colors.white,
+    marginBottom: 20,
+    overflow: "hidden",
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  subTitle: {
+    color: colors.secondary,
+    fontWeight: "bold",
+  },
+  title: {
+    marginBottom: 7,
+  },
+});
+
+export default Card;
--- app/screens/WelcomeScreen.tsx
+++ app/screens/WelcomeScreen.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text, ImageBackground, StyleSheet, Image } from "react-native";
 import Button from "../components/Button";
 
@@ -13,7 +14,7 @@ const WelcomeScreen = () => {
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
         <Text style={styles.tagLine}>Sell What You Don't Need</Text>
       </View>
-      <View style={styles.buttonsContainer}>
+      <SafeAreaView style={styles.buttonsContainer} edges={["bottom"]}>
         <Button color="primary" onPress={() => console.log("lala")}>
           LOGIN
         </Button>
@@ -21,7 +22,7 @@ const WelcomeScreen = () => {
         <Button color="secondary" onPress={() => console.log("lala")}>
           REGISTER
         </Button>
-      </View>
+      </SafeAreaView>
     </ImageBackground>
   );
 };
@@ -37,7 +38,7 @@ const styles = StyleSheet.create({
     height: 100,
   },
   buttonsContainer: {
-    padding: 20,
+    paddingHorizontal: 20,
     width: "100%",
   },
   logoCnntainer: {
